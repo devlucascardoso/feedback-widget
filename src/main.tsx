eced24af0b24f7fc3f574985abc1e2d3387f156c
Lucas Cardoso <lucascardoso946@gmail.com>
2022-05-02 17:35:34 -0300

    stage 1 completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-ReactDOM.render(
+import "./global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
